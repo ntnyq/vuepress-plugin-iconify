e0b3bbe9755bae0f01e7afe65ee5f8a369bf5b1a
ntnyq <ntnyq13@gmail.com>
2022-12-26 16:06:02 +0800

    feat: support iconify-icon

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,4 +1,3 @@
-import { h } from 'vue'
 import { defineClientConfig } from '@vuepress/client'
 import { Icon } from '@iconify/vue'
 
@@ -6,92 +5,6 @@ declare const __ICONIFY_COMPONENT_NAME__: string
 
 export default defineClientConfig({
   enhance: ({ app }) => {
-    app.component(__ICONIFY_COMPONENT_NAME__, {
-      props: {
-        icon: {
-          type: String,
-          required: true,
-        },
-
-        color: {
-          type: String,
-        },
-
-        width: {
-          type: [Number, String],
-        },
-
-        height: {
-          type: [Number, String],
-        },
-
-        inline: {
-          type: Boolean,
-          default: false,
-        },
-
-        rotate: {
-          type: [Number, String],
-        },
-
-        horizontalFlip: {
-          type: Boolean,
-          default: false,
-        },
-
-        verticalFlip: {
-          type: Boolean,
-          default: false,
-        },
-
-        flip: {
-          type: String,
-        },
-
-        horizontalAlign: {
-          type: Boolean,
-          default: false,
-        },
-
-        verticalAlign: {
-          type: Boolean,
-          default: false,
-        },
-
-        slice: {
-          type: String,
-          validator (slice: string) {
-            return [`meet`, `slice`].includes(slice)
-          },
-        },
-
-        align: {
-          type: String,
-        },
-
-        onLoad: {
-          type: Function,
-          default: () => {},
-        },
-      },
-      setup (props) {
-        return () => h(Icon, {
-          icon: props.icon,
-          color: props.color,
-          width: props.width,
-          height: props.height,
-          inline: props.inline,
-          rotate: props.rotate,
-          horizontalFlip: props.horizontalFlip,
-          verticalFlip: props.verticalFlip,
-          flip: props.flip,
-          horizontalAlign: props.horizontalAlign,
-          verticalAlign: props.verticalAlign,
-          slice: props.slice,
-          align: props.align,
-          onLoad: props.onLoad,
-        })
-      },
-    })
+    app.component(__ICONIFY_COMPONENT_NAME__, Icon)
   },
 })
