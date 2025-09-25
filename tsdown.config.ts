cd16a033ee60ad13e79865ee590c8b81795ba411
ntnyq <ntnyq13@gmail.com>
2025-09-25 10:26:47 +0800

    build: migrate to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['src/node/**/*.ts', 'src/client/**/*.ts'],
+  hash: false,
+  unbundle: true,
+})
